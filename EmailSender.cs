dc697b8897021f93d5aed1338342392f22ee15db
Hussain Khozema <96287369+as-hsm53@users.noreply.github.com>
2023-09-06 15:18:47 +0500

    First Commit

---
+++ EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace MyFirstMVC
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "hussainkhozema110@gmail.com";
+            var password = "RqaJPbnAtDH1v6UK";
+
+            var client = new SmtpClient("smtp-relay.brevo.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(mail, password)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: mail,
+                                to: email,
+                                subject,
+                                message
+                                ));
+        }
+    }
+}
